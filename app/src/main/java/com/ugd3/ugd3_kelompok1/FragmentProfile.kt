4486f661af9bdd0b43ca4eb7c12dab7964a798ff
Ace <alvincennanda@gmail.com>
2022-09-23 23:40:49 +0700

    Profile Done

--- app/src/main/java/com/ugd3/ugd3_kelompok1/FragmentProfile.kt
+++ app/src/main/java/com/ugd3/ugd3_kelompok1/FragmentProfile.kt
@@ -9,15 +9,21 @@ import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
 import android.widget.TextView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.ugd3.ugd3_kelompok1.Donasi.Home
 import com.ugd3.ugd3_kelompok1.Donasi.UserDB
+import org.w3c.dom.Text
 
 
 class FragmentProfile : Fragment(){
     private lateinit var nama: TextView
+    private lateinit var namaProfile: TextView
+    private lateinit var email: TextView
+    private lateinit var noHp: TextView
+    private lateinit var tglLahir: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +38,7 @@ class FragmentProfile : Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val btnLogout: Button = view.findViewById(R.id.btnLogout)
+        val btnEdit: Button = view.findViewById(R.id.editBtn)
 
         val db by lazy { UserDB(activity as HomeActivity) }
         val donateDao = db.donateDao()
@@ -39,11 +46,24 @@ class FragmentProfile : Fragment(){
         val sharedPreferences = (activity as HomeActivity).getSharedPreferences()
 
         val user = donateDao.getDonate(sharedPreferences.getInt("id", 0))
-        nama = view.findViewById(R.id.nama)
+        nama = view.findViewById(R.id.inputNama)
+        namaProfile = view.findViewById(R.id.inputNamaProfile)
+        email = view.findViewById(R.id.inputEmail)
+        noHp = view.findViewById(R.id.inputPhonenumber)
+        tglLahir = view.findViewById(R.id.inputTglLahir)
+
 
         // set text
         nama.setText(user.namaLengkap)
+        namaProfile.setText(user.namaLengkap)
+        email.setText(user.email)
+        noHp.setText(user.nomorTelepon)
+        tglLahir.setText(user.tanggalLahir)
 
+        btnEdit.setOnClickListener{
+            val moveEditBtn = Intent(this@FragmentProfile.context, EditProfileActivity::class.java)
+            startActivity(moveEditBtn)
+        }
 
 
         btnLogout.setOnClickListener {
