47ee1e90d0dc995036851947b5117875990c14e1
anitaharyodi <63808433+anitaharyodi@users.noreply.github.com>
2022-09-24 11:32:50 +0700

    update profile done

--- app/src/main/java/com/ugd3/ugd3_kelompok1/FragmentHome.kt
+++ app/src/main/java/com/ugd3/ugd3_kelompok1/FragmentHome.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputLayout
 import com.ugd3.ugd3_kelompok1.Donasi.Home
+import com.ugd3.ugd3_kelompok1.Donasi.UserDB
 
 
 class FragmentHome : Fragment() {
+    private lateinit var namaView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +28,18 @@ class FragmentHome : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val layoutManager = LinearLayoutManager(context)
         val adapter: RVHomeAdapter = RVHomeAdapter(Home.listOfHome)
+        namaView = view.findViewById(R.id.namaView)
 
         val rvHome : RecyclerView = view.findViewById(R.id.rv_home)
+
+        val db by lazy { UserDB(activity as HomeActivity) }
+        val donateDao = db.donateDao()
+
+        val sharedPreferences = (activity as HomeActivity).getSharedPreferences()
+
+        val user = donateDao.getDonate(sharedPreferences.getInt("id", 0))
+
+        namaView.text = user.namaLengkap
 
         //Set layout manager dari recycler view
         rvHome.layoutManager = layoutManager
