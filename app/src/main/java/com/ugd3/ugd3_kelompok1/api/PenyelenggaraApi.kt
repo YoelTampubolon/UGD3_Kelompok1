708232f829d4cd24a3097d07ab2101fe9bfc67ee
Ace <alvincennanda@gmail.com>
2022-12-16 22:09:39 +0700

    Hosting

--- app/src/main/java/com/ugd3/ugd3_kelompok1/api/PenyelenggaraApi.kt
+++ app/src/main/java/com/ugd3/ugd3_kelompok1/api/PenyelenggaraApi.kt
@@ -2,7 +2,8 @@ package com.ugd3.ugd3_kelompok1.api
 
 class PenyelenggaraApi {
     companion object {
-        val BASE_URL = "http://192.168.0.100/UGD_Kelompok1/public/api/"
+//        val BASE_URL = "http://192.168.0.100/UGD_Kelompok1/public/api/"
+        val BASE_URL = "http://kasihh.my.id/api/"
 
         val GET_ALL_URL = BASE_URL + "penyelenggara"
         val GET_BY_ID_URL = BASE_URL + "penyelenggara/"
