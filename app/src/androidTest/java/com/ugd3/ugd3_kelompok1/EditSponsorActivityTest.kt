8aec3a53eb76c3d1ed0de59efca9e4ced79ec51d
Yoel Otniel P.M.T (200710753) <yoeltampubolon457@gmail.com>
2022-12-16 16:23:17 +0700

    Sponsor Test

---
+++ app/src/androidTest/java/com/ugd3/ugd3_kelompok1/EditSponsorActivityTest.kt
@@ -0,0 +1,223 @@
+package com.ugd3.ugd3_kelompok1
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class EditSponsorActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(EditSponsorActivity::class.java)
+
+    @Test
+    fun editSponsorActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.ll_button),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.et_namaSponsor),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_namaSponsor),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText.perform(scrollTo(), replaceText("RomanCoffee"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.ll_button),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.et_usia),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_usia),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText2.perform(scrollTo(), replaceText("65 Tahun"), closeSoftKeyboard())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.ll_button),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton3.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText3 = onView(
+            allOf(
+                withId(R.id.et_nominal),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_nominal),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText3.perform(scrollTo(), replaceText("45000000"), closeSoftKeyboard())
+
+        val materialButton4 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.ll_button),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton4.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+
+        val textInputEditText4 = onView(
+            allOf(
+                withId(R.id.et_tujuanDonasi),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_tujuanDonasi),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText4.perform(scrollTo(), replaceText("250000000"), closeSoftKeyboard())
+
+        val materialButton5 = onView(
+            allOf(
+                withId(R.id.btn_save), withText("Simpan"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.ll_button),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton5.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    fun waitFor(delay: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for " + delay + "milliseconds"
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+        }
+    }
+}
